b3f13392529b2e8da5e621387d79b0c78a541e90
Johan Walles <johan.walles@gmail.com>
2017-12-04 21:58:46 +0100

    Post crashes to Crashlytics

--- app/src/main/java/com/gmail/walles/johan/cleverdrawer/Application.java
+++ app/src/main/java/com/gmail/walles/johan/cleverdrawer/Application.java
@@ -1,10 +1,14 @@
 package com.gmail.walles.johan.cleverdrawer;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class Application extends android.app.Application {
     @Override
     public void onCreate() {
+        Fabric.with(this, new Crashlytics());
         Timber.plant(new Timber.DebugTree());
 
         super.onCreate();
