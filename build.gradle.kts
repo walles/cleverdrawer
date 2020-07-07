13e3fd22b104472ff3346e65a6e26803f729110f
Johan Walles <johan.walles@gmail.com>
2020-07-07 19:28:11 +0200

    Updates based on various Internet findings

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.0")
+        classpath(kotlin("gradle-plugin", version = "1.3.70"))
         classpath("com.google.gms:google-services:4.3.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.2.0")
 
@@ -48,6 +49,6 @@ allprojects {
     }
 }
 
-task("clean") {
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
